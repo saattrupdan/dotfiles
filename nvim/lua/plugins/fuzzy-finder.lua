11e76731a7de41ae2b120be2098d8e0d548f1703
saattrupdan <saattrupdan@gmail.com>
2024-12-09 12:17:22 +0100

    chore: Update dot files

--- nvim/lua/plugins/fuzzy-finder.lua
+++ nvim/lua/plugins/fuzzy-finder.lua
@@ -17,5 +17,25 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+          ensure_installed = {
+            "lua",
+            "vim",
+            "vimdoc",
+            "javascript",
+            "typescript",
+            "html",
+            "css",
+            "vue",
+            "python",
+            "fsharp",
+          },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },
+        })
+    end
   },
 }
