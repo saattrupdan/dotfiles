29cef8b3ad4a693d04b81aed9edf4789733a76f3
saattrupdan <saattrupdan@gmail.com>
2025-11-24 21:13:02 +0100

    chore: Update dot files

---
+++ nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,61 @@
+-- Open the chat
+vim.keymap.set('n', '<leader>cc', ':CodeCompanionChat<CR>')
+
+return {
+  "olimorris/codecompanion.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "neoclide/coc.nvim" },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  },
+  opts = {
+    display = {
+      chat = {
+        window = {
+          position = "right",
+          width = 88,
+        },
+      },
+    },
+    memory = {
+      opts = {
+        chat = {
+          enabled = true,
+        },
+      },
+    },
+    strategies = {
+      chat = {
+        -- adapter = "ollama",
+        -- model = "qwen3-coder:30b",
+        variables = {
+          ["buffer"] = {
+            opts = {
+              default_params = 'pin'
+            },
+          },
+        },
+        tools = {
+          opts = {
+            auto_submit_errors = true,
+            auto_submit_success = true,
+            default_tools = {
+              "full_stack_dev",
+            },
+          },
+        },
+        opts = {
+          completion_provider = "coc",
+        },
+      },
+      -- inline = {
+      --   adapter = "ollama",
+      --   model = "qwen3-coder:30b",
+      -- },
+    },
+    opts = {
+      log_level = "DEBUG"
+    },
+  },
+}
