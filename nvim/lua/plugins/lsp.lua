a809fe84ae0d0bf3cc026c135c676f64d48e4c96
saattrupdan <saattrupdan@gmail.com>
2025-11-29 18:28:55 +0100

    chore: Update dot files

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,12 +4,11 @@ return {
     "mason-org/mason.nvim",
     "mason-org/mason-lspconfig.nvim",
   },
+  build = ":MasonInstall pyrefly",
   config = function()
     require("mason").setup()
     require("mason-lspconfig").setup {
-      ensure_installed = {
-        "pyrefly"
-      },
+      ensure_installed = { "pyrefly" },
     }
     vim.lsp.enable({"pyrefly"})
     vim.lsp.inlay_hint.enable(false)
