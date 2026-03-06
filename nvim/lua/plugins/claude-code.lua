ce3d975661234e02874af96e08300db94eccd880
saattrupdan <saattrupdan@gmail.com>
2026-03-06 13:30:07 +0100

    chore: Update dot files

--- nvim/lua/plugins/claude-code.lua
+++ nvim/lua/plugins/claude-code.lua
@@ -10,7 +10,13 @@ return {
       window = {
         position = "float",
       },
-      command = "ANTHROPIC_AUTH_TOKEN=ollama ANTHROPIC_BASE_URL=http://127.0.0.1:1234 claude --model qwen/qwen3.5-35b-a3b --tools default",
+      command = "ANTHROPIC_AUTH_TOKEN=lmstudio ANTHROPIC_BASE_URL=http://127.0.0.1:1234 claude --model qwen/qwen3.5-35b-a3b --tools default",
+      keymaps = {
+        toggle = {
+          normal = "<C-,>",
+          terminal = "<C-,>",
+        },
+      },
     })
   end
 }
