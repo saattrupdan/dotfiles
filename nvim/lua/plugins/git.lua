d2b0efcae638783891a628fa272ecc71435817e8
saattrupdan <saattrupdan@gmail.com>
2026-04-07 12:24:18 +0200

    chore: Update dot files

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,17 +1,5 @@
 vim.g.merginal_remoteVisible = 0
 
--- Show current branch in lightline status bar
-vim.g.lightline = {
-  colorscheme = "one",
-  active = {
-    left = {
-      { "mode", "paste" },
-      { "gitbranch", "readonly", "filename", "modified" },
-    },
-  },
-  component_function = { gitbranch = "fugitive#head" },
-}
-
 vim.keymap.set("n", "<leader>gs", ":Git<cr>")
 vim.keymap.set("n", "<leader>gd", ":Gdiff<cr>")
 vim.keymap.set("v", "<leader>dg", ":diffget<cr>")
@@ -21,5 +9,4 @@ vim.keymap.set("n", "<leader>gb", ":Merginal<cr>")
 return {
   { "tpope/vim-fugitive" },
   { "idanarye/vim-merginal", dependencies = { "tpope/vim-fugitive" } },
-  { "itchyny/lightline.vim", dependencies = { "tpope/vim-fugitive" } },
 }
