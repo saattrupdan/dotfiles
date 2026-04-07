c7c89bd49ff9d62e25ee77523271aefd6be37f25
saattrupdan <saattrupdan@gmail.com>
2026-04-07 12:44:33 +0200

    chore: Update dot files

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -23,7 +23,8 @@ vim.keymap.set('n', '<leader><cr>', 'gwgw')
 vim.keymap.set('v', '<leader><cr>', 'gw<cr>')
 
 -- Switch from Terminal mode to Normal mode
-vim.keymap.set('t', '<C-B>[', '<C-\\><C-n>')
+-- vim.keymap.set('t', '<C-B>[', '<C-\\><C-n>')
+vim.keymap.set('t', '<esc>', '<C-\\><C-n>')
 
 -- Breakpoints in Python
 vim.keymap.set("n", "<leader><leader>", "obreakpoint()<esc>:w<cr>")
