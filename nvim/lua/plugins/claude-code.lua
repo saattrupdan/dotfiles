abae45891952f948c1a6e8681f48868021fe15fd
saattrupdan <saattrupdan@gmail.com>
2026-04-16 08:58:02 +0200

    chore: Update dot files

--- nvim/lua/plugins/claude-code.lua
+++ nvim/lua/plugins/claude-code.lua
@@ -10,11 +10,11 @@ return {
       window = {
         position = "float",
       },
-      command = "ANTHROPIC_AUTH_TOKEN=llamacpp ANTHROPIC_BASE_URL=http://127.0.0.1:8080 CLAUDE_CODE_ENABLE_TELEMETRY=0 CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC=1 CLAUDE_CODE_ATTRIBUTION_HEADER=0 CLAUDE_CODE_ENABLE_PROMPT_SUGGESTION=false claude --model Qwen3.5-35B-A3B-Q4_K_M --tools default --dangerously-skip-permissions",
+      command = "CLAUDE_CODE_ENABLE_TELEMETRY=0 CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC=1 CLAUDE_CODE_ATTRIBUTION_HEADER=0 CLAUDE_CODE_ENABLE_PROMPT_SUGGESTION=false claude --tools default --dangerously-skip-permissions",
       keymaps = {
         toggle = {
-          normal = "<C-,>",
-          terminal = "<C-,>",
+          normal = "<C-.>",
+          terminal = "<C-.>",
         },
         window_navigation = false,
         scrolling = false,
