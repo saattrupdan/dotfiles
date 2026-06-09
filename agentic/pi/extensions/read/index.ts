0e31f39612ffd9cb7bc372ef99af174e702c8753
saattrupdan <saattrupdan@gmail.com>
2026-06-09 14:28:34 +0200

    chore: remove unused unzip import from read extension

--- agentic/pi/extensions/read/index.ts
+++ agentic/pi/extensions/read/index.ts
@@ -27,7 +27,6 @@ import * as crypto from "node:crypto";
 import * as fs from "node:fs";
 import * as os from "node:os";
 import * as path from "node:path";
-import { unzip } from "node:zlib";
 
 import {
 	type AgentToolResult,
