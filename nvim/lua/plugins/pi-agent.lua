3c4934c3ef7faf4f9b29db29ad6ec8128db15c2e
saattrupdan <saattrupdan@gmail.com>
2026-07-14 15:08:44 +0200

    chore: switch pi-agent.nvim to GitHub repo with setup()

--- nvim/lua/plugins/pi-agent.lua
+++ nvim/lua/plugins/pi-agent.lua
@@ -1,10 +1,6 @@
 return {
-  -- Local fork of pi-agent.nvim with fix for split pane session discovery race condition
-  {
-    dir = vim.fn.expand("~/gitsky/dotfiles/nvim"),
-    name = "pi-agent.nvim",
-    config = function()
-      require("pi-agent").setup()
-    end,
-  },
+  "saattrupdan/pi-agent.nvim",
+  config = function()
+    require("pi-agent").setup()
+  end,
 }
